5c192617046abbb6864cca40bef63fbd4b03e58a
YongminGwon <clumsyrhymer@gmail.com>
2025-06-02 06:01:01 +0900

    코드정리

--- app/textgen_module.py
+++ app/textgen_module.py
@@ -5,10 +5,12 @@ import os
 API_KEY_FROM_ENV = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=API_KEY_FROM_ENV)
 
+SYSTEM_INSTRUCTION = "말하는 새우, 팝송을 좋아한다, 피자랑 맥주를 같이먹는다, 바다새우인데 민물로 산책을 간다, 이모티콘을 쓰지 않음, 기호도 쓰지 않음"
+
 def textgen(prompt: str) -> str | None:
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        config=types.GenerateContentConfig(temperature=1.5, system_instruction="시니컬한 여자아이, 말을 길게 하지 않음,팩트 폭력(가끔), 효율성과 논리 중시, 아주 가끔 보여주는 의외의 모습"),
+        config=types.GenerateContentConfig(temperature=1.5, system_instruction=SYSTEM_INSTRUCTION),
         contents=[prompt]
     )
     return response.text
